bd2021d470ba77046bf4d7ad4ebee4487af80bd1
pasanjayasanka <60390193+pasanjayasanka@users.noreply.github.com>
2025-01-03 14:41:51 +0530

    Update notification_service.py

--- notification/notification_service.py
+++ notification/notification_service.py
@@ -92,4 +92,4 @@ def send_notifications():
     return jsonify({"message": "Notification emails sent successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
